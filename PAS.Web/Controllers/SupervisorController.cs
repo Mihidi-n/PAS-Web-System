3eba69d30324454b698e39a53418d52d48c3a16d
keshari-w <kesharirochana@gmail.com>
2026-04-18 18:35:26 +0530

    Add supervisor expertise selection feature
    Added functionality for supervisors to select and save their research expertise areas.
    
    Includes GET and POST methods to load research areas and store selected areas in the database.

--- PAS.Web/Controllers/SupervisorController.cs
+++ PAS.Web/Controllers/SupervisorController.cs
@@ -1,1 +1,30 @@
-﻿
+﻿[HttpGet]
+public async Task<IActionResult> Expertise()
+{
+    var researchAreas = await _context.ResearchAreas.OrderBy(r => r.Name).ToListAsync();
+    ViewBag.ResearchAreas = new MultiSelectList(researchAreas, "Id", "Name");
+    return View();
+}
+
+[HttpPost]
+[ValidateAntiForgeryToken]
+public async Task<IActionResult> Expertise(List<int> selectedAreas)
+{
+    var supervisorId = User.FindFirstValue(ClaimTypes.NameIdentifier)!;
+
+    var oldItems = _context.SupervisorExpertise.Where(x => x.SupervisorId == supervisorId);
+    _context.SupervisorExpertise.RemoveRange(oldItems);
+
+    foreach (var areaId in selectedAreas)
+    {
+        _context.SupervisorExpertise.Add(new SupervisorExpertise
+        {
+            SupervisorId = supervisorId,
+            ResearchAreaId = areaId
+        });
+    }
+
+    await _context.SaveChangesAsync();
+    TempData["Success"] = "Expertise areas saved successfully.";
+    return RedirectToAction(nameof(Dashboard));
+}
