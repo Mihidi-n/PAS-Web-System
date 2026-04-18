175967695da176dd019db7b334796d5377e76ffd
savidiXX <hemaranjanee7575@gmail.com>
2026-04-18 21:22:31 +0530

    Add student dashboard to display project proposals

--- PAS.Web/Controllers/StudentController.cs
+++ PAS.Web/Controllers/StudentController.cs
@@ -1,1 +1,32 @@
-﻿
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PAS.Web.Data;
+using PAS.Web.Models;
+using PAS.Web.ViewModels;
+
+namespace PAS.Web.Controllers;
+
+[Authorize(Roles = "Student")]
+public class StudentController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public StudentController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Dashboard()
+    {
+        var studentId = User.FindFirstValue(ClaimTypes.NameIdentifier)!;
+
+        var proposals = await _context.ProjectProposals
+            .Include(p => p.ResearchArea)
+            .Where(p => p.StudentId == studentId)
+            .ToListAsync();
+
+        return View(proposals);
+    }﻿
