7a8122ecfb8b5e2dc2a75c48b2b524cd00be56f7
senethma-kolonnage <senethmamadushi@gmail.com>
2026-04-19 11:58:47 +0530

    Added Supervisor Dashboard action to display anonymous project proposals
    Implemented Dashboard method to retrieve and display anonymous projects for supervisors

--- PAS.Web/Controllers/SupervisorController.cs
+++ PAS.Web/Controllers/SupervisorController.cs
@@ -1,4 +1,33 @@
-﻿[HttpGet]
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using PAS.Web.Data;
+using PAS.Web.Models;
+using PAS.Web.Services.Interfaces;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PAS.Web.Controllers;
+
+[Authorize(Roles = "Supervisor")]
+public class SupervisorController : Controller
+{
+    private readonly IMatchingService _matchingService;
+    private readonly ApplicationDbContext _context;
+
+    public SupervisorController(IMatchingService matchingService, ApplicationDbContext context)
+    {
+        _matchingService = matchingService;
+        _context = context;
+    }
+
+    public async Task<IActionResult> Dashboard()
+    {
+        var supervisorId = User.FindFirstValue(ClaimTypes.NameIdentifier)!;
+        var projects = await _matchingService.GetAnonymousProjectsForSupervisorAsync(supervisorId);
+        return View(projects);
+    }
+[HttpGet]
 public async Task<IActionResult> Expertise()
 {
     var researchAreas = await _context.ResearchAreas.OrderBy(r => r.Name).ToListAsync();
