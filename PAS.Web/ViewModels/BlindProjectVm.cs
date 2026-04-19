fe6befd890df102452e1d4e8b5fca41e7c872f57
vimarshanananayakkara2 <vimarshanananayakkara2@gmail.com>
2026-04-19 05:58:41 +0530

    Created BlindProjectVm for anonymous project representation in supervisor blind review

--- PAS.Web/ViewModels/BlindProjectVm.cs
+++ PAS.Web/ViewModels/BlindProjectVm.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using PAS.Web.Models;
+
+namespace PAS.Web.ViewModels;
+
+public class BlindProjectVm
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Abstract { get; set; } = string.Empty;
+    public string TechnicalStack { get; set; } = string.Empty;
+    public string ResearchAreaName { get; set; } = string.Empty;
+    public ProposalStatus Status { get; set; }
+}
